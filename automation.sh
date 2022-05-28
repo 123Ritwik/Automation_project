c5ab97380e4d4dc043465b8ad0ecaa2f5c46f36e
123Ritwik <103556810+123Ritwik@users.noreply.github.com>
2022-05-28 22:30:44 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -35,3 +35,20 @@ tar -cf /tmp/${name}-httpd-logs-${timestamp}.tar *.log
 	aws s3 cp /tmp/${name}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${name}-httpd-logs-${timestamp}.tar
 
 
+logfile="/var/www/html"
+
+if [[ ! -f ${logfile}/inventory.html ]]; then
+	
+	echo -e 'Log Type\t-\tTime Created\t-\tType\t-\tSize' > ${logfile}/inventory.html
+fi
+
+if [[ -f ${logfile}/inventory.html ]]; then
+	
+    size=$(du -h /tmp/${name}-httpd-logs-${timestamp}.tar | awk '{print $1}')
+	echo -e "httpd-logs\t-\t${timestamp}\t-\ttar\t-\t${size}" >> ${logfile}/inventory.html
+fi
+
+if [[ ! -f /etc/cron.d/automation ]]; then
+	
+	echo "* * * * * root /root/automation.sh" >> /etc/cron.d/automation
+fi
